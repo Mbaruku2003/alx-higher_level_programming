00a0a0c2b3815019e16431fb744e6e6b96a1e91f
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-11 07:13:44 -0700

    10-a when dealing with linked lists, we create the tag types and pointers with *

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <strings.h>
+/**
+ * check_cycle - checks for a circular list
+ * @list: the list being checked
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *oncemove;
+	listint_t *twicemove;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	oncemove = list;
+	twicemove = list;
+	while (oncemove != NULL && twicemove != NULL && twicemove->next != NULL)
+	{
+		oncemove = oncemove->next;
+		twicemove = twicemove->next;
+
+		if (oncemove == twicemove)
+		{
+			return (1);
+		}
+	}
+	return (-1);
+}
