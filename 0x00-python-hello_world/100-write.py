ad04cd66db5a7d132f67302153e7862bfe32199a
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-11 07:46:37 -0700

    100-write.py trial

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import sys#inports the sys module#
-#sys.stdout.write, writes output to the standard output
-sys.stdout.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
-#sys.exit eits the sys module with an arguenment
+import sys
+sys.error.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
-#sys.stdin reads input from commandline
