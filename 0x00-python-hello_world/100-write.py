acecdfbee735c78f1e55a20f1a988a597d667b49
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-11 07:34:28 -0700

    100-write.py

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys#inports the sys module#
+#sys.stdout.write, writes output to the standard output
+sys.stdout.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+#sys.exit eits the sys module with an arguenment
+sys.exit(1)
+#sys.stdin reads input from commandline
