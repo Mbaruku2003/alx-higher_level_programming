a5449769a6fc02c62eca0e1917c3639dd16bcf7b
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-12 05:47:51 -0700

    one can useif and else statement in ne line in python

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+once = abs(number) % 10
+sign = "-" if number < 0 else""
+print(f"Last digit of {number} is {sign}{once}", end=" ")
+if number % 10 > 5:
+    print(f"and is greater than 5")
+elif number % 10 == 0:
+    print(f"and is 0")
+else:
+    print(f"and is less than 6 and not 0")
