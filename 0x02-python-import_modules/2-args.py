0b5f37330cafae3ba6b62e1447914f3dfcef6420
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 06:23:57 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     leo = len(argv) - 1
     if leo < 1:
-        print("{} arguements.".format(leo))
+        print("{} arguments.".format(leo))
     elif leo == 1:
         print("{} argument:".format(leo))
     else:
