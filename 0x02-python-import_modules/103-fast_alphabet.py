95b0afe9d3ccece440d1cc4de643486f0519b84e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 12:40:41 -0700

    103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import string
-    uppercase_letters = string.ascii_uppercase
-    print("{}".format(uppercase_letters), end="")
-    print()
+print("ABCDEFGHIJKLMNOPQRSTUVWXYZ", end="")
+print()
