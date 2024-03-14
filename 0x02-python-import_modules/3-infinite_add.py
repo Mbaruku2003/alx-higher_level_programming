c02c05f0220b6f45c75eed13e40e807ad81c55f6
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 09:02:22 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    string = 0
+    length = len(argv)
+    for count in range(1, length):
+        string += int(argv[count])
+        print("{}".format(string))
