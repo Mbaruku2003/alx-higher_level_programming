2929cc3bd607f1417a9048c4c874b5caa91900f0
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 15:14:37 -0700

    know wich condition must be an else not all statements fit being an else

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,35 +5,33 @@ if __name__ == "__main__":
     leo = len(argv)
     if leo != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="")
-        print()
-        exit(1)
+        exit (1)
     operator = argv[2]
     a = argv[1]
     b = argv[3]
-    if opearator != add or sub or mul or div:
-        print("Unknown operator. Available operators: +, -, * and /", end="")
-        print()
-        exit(1)
     leo = int(a)
     mbaruk = int(b)
-    if operator is add:
+    if operator == '+':
         result = a + b
         print("{} + {} = {}".format(leo, mbaruk, result), end="")
         print()
         exit(1)
-    elif operator is sub:
+    elif operator == '-':
         result = a - b
         print("{} + {} = {}".format(leo, mbaruk, result), end="")
         print()
         exit(1)
-    elif operator is mul:
+    elif operator == '*':
         result = a * b
         print("{} * {} = {}".format(leo, mbaruk, result), end="")
         print()
         exit (1)
-    else:
+    elif operator == "\\":
         result = a / b
         print("{} / {} = {}".format(leo, mbaruk, result), end="")
         print()
         exit (1)
-
+    else:
+        print("Unknown operator. Available operators: +, -, * and /", end="")
+        print()
+        exit (1)
