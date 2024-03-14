f37fad8a5aa66de7af6c93bb732ebc294709a022
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 09:31:18 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     moddef = dir()
     for length_moddef in range(0, len(moddef)):
         if moddef[length_moddef][:2] != "__":
-            print("{:s}".format(moddef[length_moddef))
+            print("{:s}".format(moddef[length_moddef]))
