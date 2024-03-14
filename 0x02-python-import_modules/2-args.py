8139c8bee8376896430268aa3694d0dbab04c199
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 06:06:03 -0700

    {:s} is used for strings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     leo = len(argv) - 1
     if leo <= 0:
-        print("{:d} arguements".format(leo))
+        print("{:d} arguements.".format(leo))
     elif leo == 1:
         print("{:d} argument:".format(leo))
         print("{:d}: {:s}".format(leo, argv[leo]))
