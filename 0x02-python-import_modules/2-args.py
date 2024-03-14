6d24ac95957c56e90693c620b0fe2b73554c918c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-14 06:18:16 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     leo = len(argv) - 1
-    if leo <= 0:
-        print("{:d} arguements.".format(leo))
+    if leo < 1:
+        print("{} arguements.".format(leo))
     elif leo == 1:
-        print("{:d} argument:".format(leo))
-        print("{:d}: {:s}".format(leo, argv[leo]))
+        print("{} argument:".format(leo))
     else:
-        print("{:d} arguments:".format(leo))
-        leo = leo + 1
-        for leo in range(1,7):
-            print("{:d}: {:s}".format(leo, argv[leo]))
+        print("{} arguments:".format(leo))
+
+    for numbering in range(leo):
+        print("{}: {:s}".format(numbering + 1, argv[numbering] + 1))
