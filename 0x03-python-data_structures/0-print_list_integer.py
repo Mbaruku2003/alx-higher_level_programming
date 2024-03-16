a2c21e3a9a7eb33602b401e7764dd963b7c38b4b
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-16 03:05:58 -0700

    we use a for loop to iterate through a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    leo = len(my_list) + 1
-    for i in range(1, leo):
-        print("{}".format(i), end="\n")
+    for x in my_list:
+        print("{:d}".format(x), end="\n")
