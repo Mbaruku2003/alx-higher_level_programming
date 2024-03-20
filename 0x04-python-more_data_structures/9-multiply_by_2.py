c1444aab2f6dd93312a38aed7907cb909a295422
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-20 08:38:08 -0700

    9-main.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    if a_dictionary:
+        for key, v in a_dictionary.items():
+            new_dictionary.update({key: v * 2})
+    return new_dictionary
