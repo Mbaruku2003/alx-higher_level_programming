5ecdaa5bcdb9701fcf1211d1ead1c24d6ab7c8e6
Mbaruku2003 <leonardjembe21@gmail.com>
2024-03-21 03:20:02 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * h: the header for the linked list
+ * Return: the number of nodes in the linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+	count = 0;
+
+	if (h == NULL)
+	{
+		return count;
+	}
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return count;
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+doubly linked lists are lists that have both frward and backward movement
+a head in alinked list is the node with the pointer previous equals to NULL
