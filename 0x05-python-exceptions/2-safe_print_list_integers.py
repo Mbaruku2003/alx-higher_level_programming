96fc8ce466321120eb0d46b3cfcd6532e5307122
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-02 03:40:43 -0700

    typeerror tests for wheater we ar comparing things of diff types or using an unsupported perator

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     number = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             number += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             pass
     print()
     return number
