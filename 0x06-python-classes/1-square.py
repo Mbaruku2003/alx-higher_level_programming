db409af68dbdd93c3a8ae531350d6ee193e76f3b
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-02 12:46:55 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,4 +3,8 @@
 class Square:
     """represents a square"""
     def __init__(self, size):
+        """constructor.
+        Args:
+        size: length of sides of the square
+        """
         self.__size = size
