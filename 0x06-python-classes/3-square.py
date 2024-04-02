570626f1fb6aaeb6354807e3f70d82eeb618f645
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-02 15:35:14 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/bin/python3
-"""Defining a class"""
+"""Defining a class."""
 
 
 class Square:
-    """Defining a method init"""
+    """Defining a method init."""
     def __init__(self, size=0):
+        """Defining a new object."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+        """Return the area."""
         return (self.__size * self.__size)
