63eebdc1771df60a08d7a28ec52109843547a6a7
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-08 04:28:07 -0700

    created 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """let ass pass to create a class without error even though nothing has been written"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+static methods dont have an instance or a class
+class methods have the class
