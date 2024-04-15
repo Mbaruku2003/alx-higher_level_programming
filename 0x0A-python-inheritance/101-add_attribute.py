207692b178de3cf4bff3d49c0359bd1fe29460e0
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-15 12:41:26 -0700

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class."""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """Define an attribute."""
+
+    if hasattr(obj, attr_name):
+        raise TypeError("can't add new attribute{}".format(attr_name))
+    setattr(obj, attr_name, attr_value)
