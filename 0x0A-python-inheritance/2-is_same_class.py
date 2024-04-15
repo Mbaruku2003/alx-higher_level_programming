ef86abe4094fca7a1db3e954639c087e5b233daf
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-15 04:46:18 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a module."""
 
+
 def is_same_class(obj, a_class):
     """Returns a booleon expression."""
-    return type(obj) == a_class
+    return type(obj) is a_class
