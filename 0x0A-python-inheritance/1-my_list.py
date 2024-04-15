885344b3197b9dc1778ec46f1f83cdef42ab1bef
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-15 04:38:02 -0700

    type(obj) checks wheather one class is of the previous type

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class."""
+
+
+class MyList:
+    """Define a function."""
+
+    def print_sorted(self):
+        """Printed list."""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a module."""
+
+
+def is_same_class(obj, a_class):
+    """
+    Define what the function will return
+    we will use type to test wheather object
+    and a-class are of same type.
+    """
+    return type(obj) == a_class
