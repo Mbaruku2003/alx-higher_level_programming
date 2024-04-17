2c5ccd8ab793b6e34a61851abafc3f70a64b29e0
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-17 07:42:18 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 def inherits_from(obj, a_class):
     """Return True or False"""
 
-    return (isinstance(obj, a_class) else type(obj) is not a_class)
+    return (isinstance(obj, a_class) and type(obj) is not a_class)
