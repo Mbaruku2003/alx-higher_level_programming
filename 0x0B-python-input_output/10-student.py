55415657720c05359e88dbe5ded8c1b404339155
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-17 05:39:05 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,6 +4,7 @@
 
 class Student:
     def __init__(self, first_name, last_name, age):
+        """Instantiate a string."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
