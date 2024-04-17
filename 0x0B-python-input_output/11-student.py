179ad66068b79e4a2814b419ab64a920a6ce5862
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-17 06:15:14 -0700

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,7 +7,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         self.age = age
-        self.fisrt_name = first_name
+        self.first_name = first_name
         self.last_name = last_name
 
     def to_json(self, attrs=None):
