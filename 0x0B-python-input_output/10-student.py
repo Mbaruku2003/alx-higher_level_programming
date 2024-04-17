d2c295ab1b281c2feb10d8553fdc7c898f3e9fd4
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-17 05:40:09 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,11 +5,14 @@
 class Student:
     def __init__(self, first_name, last_name, age):
         """Instantiate a string."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Turns a string to json."""
+
         if (type(attrs) == list and
                 all(type(element) == str for element in attrs)):
             return {k:getattr(self, k) for k in attrs if hasattr(self, k)}
