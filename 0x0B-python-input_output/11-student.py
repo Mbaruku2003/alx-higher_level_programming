98b742b3cae0aab6f87c681b4ce6fa28c5028f48
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-17 06:48:33 -0700

    101-generator.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,9 +6,9 @@ class Student:
     """Define instantation."""
 
     def __init__(self, first_name, last_name, age):
-        self.age = age
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
 
