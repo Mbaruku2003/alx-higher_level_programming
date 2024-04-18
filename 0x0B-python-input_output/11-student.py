51529ae65b2293845e5fa35ff82730b91d92e6b3
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-18 10:31:28 -0700

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,8 +11,11 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-
-        if  (type(attrs) == list and 
-                all(type(element) == str for element in attrs)):
+        if attrs == None:
+            return self.__dict__
+        else:
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
