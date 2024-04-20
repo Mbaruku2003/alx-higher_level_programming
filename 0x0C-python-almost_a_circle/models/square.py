76099ab5c7e3a910c81df670a1ffb952796a2253
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-20 06:52:14 -0700

    it is only in places where two sizes are needed that we put self.width and self.eight

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,39 @@ class Square(Rectangle):
 
     def __str__(self):
         return ("[square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args:
+            for index, value in enumerate(args):
+                if index == 0:
+                    self.id = value
+                if index == 1:
+                    self.size = value
+                if index == 2:
+                   self.x = value
+                if index == 3:
+                    self.y = value
+                else:
+                    continue
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y =  value
+                else:
+                    break
