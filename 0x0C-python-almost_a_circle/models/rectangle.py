aa41db4725f164d13112918703b156108e41711f
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-23 02:18:21 -0700

    r

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,14 +15,10 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Returns the width."""
-
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width."""
-
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -31,14 +27,10 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Return the heught."""
-
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height."""
-
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -47,14 +39,10 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Return the value of x."""
-
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set the value of x."""
-
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -63,14 +51,10 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Get the value of y."""
-
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set the value of y."""
-
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -78,27 +62,19 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """Get the area."""
-
         return (self.__height * self.__width)
 
     def display(self):
-    """Display # for the funtion."""
-
         for j in range(self.__y):
             print()
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        """Define str."""
-
         return ("[Rectangle] " + "{} {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height))
 
     def update(self, *args, **kwargs):
-        """Update the id."""
-
         if args:
             for index, value in enumerate(args):
                 if index == 0:
@@ -129,8 +105,6 @@ class Rectangle(Base):
                     break
 
     def to_dictionary(self):
-        """Define a dictionary."""
-
         return {"id": self.id,
                 "width": self.width,
                 "height": self.height,
