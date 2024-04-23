c997de07e4d5709961bc973447bfc51503103d66
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-23 06:35:25 -0700

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,21 +6,30 @@ class Square(Rectangle):
     """Define the constructor."""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Define the constructor."""
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Define the constructor."""
+
         return ("[square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """Define the constructor."""
+
         return self.width
 
     @size.setter
     def size(self, value):
+        """Define the constructor."""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Define the constructor."""
+
         if args:
             for index, value in enumerate(args):
                 if index == 0:
@@ -48,6 +57,8 @@ class Square(Rectangle):
                     break
 
     def to_dictionary(self):
+        """Define the constructor."""
+
         dictionary = {"id": self.id,
                 "x": self.x,
                 "size": self.size,
