6e6f4f9c0bd2851b6e0b6ee909791f480d9bad4c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-21 06:18:37 -0700

    try

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- deletes the database hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 doesn’t exist
+-- not allowed to use the SELECT or SHOW statements
+DELETE database IF EXISTS hbtn_0c_0
