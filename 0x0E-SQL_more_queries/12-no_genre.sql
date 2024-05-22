0fa7c2d364cda6a45ad4e4e3d16229247e8fd400
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 11:44:28 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
---  lists all shows contained in hbtn_0d_tvshows without a genre linked
+--  lists all shows in hbtn_0d_tvshows without a genre
 SELECT title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ONid=tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY title, tv_show_genres.genre_id;
