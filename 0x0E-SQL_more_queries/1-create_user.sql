ce73bbbd2c77c226ffb65b5c4e96f55e8b431164
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 06:24:37 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 -- user_0d_1 password should be set to user_0d_1_pwd
 -- user_0d_1 already exists, your script should not fail
 CREATE IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
