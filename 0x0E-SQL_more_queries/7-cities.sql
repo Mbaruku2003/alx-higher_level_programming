ee94c71201d494d5565224d094fb86988da366b5
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 09:26:57 -0700

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS cities (
+CREATE table IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL;
