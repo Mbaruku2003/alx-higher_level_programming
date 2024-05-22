faa652daa52f0dcdf0c23d36ea4f394f3a45ae79
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 07:05:20 -0700

    3-force_name.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- creates the table force_name
+-- forc_name description id INT and name(VARCHAR(256) AND CANT BE NULL
+-- If the table force_name already exists, your script should not faiL
+CREATE table IF NOT EXISTS force_name (id INT, name VARCHAR(256))
