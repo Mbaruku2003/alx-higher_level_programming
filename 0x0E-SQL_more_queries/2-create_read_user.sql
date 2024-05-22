f2fbff6805b16978db1a7c22341b007f3326c0e6
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 05:59:04 -0700

    aupdated 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,8 +3,8 @@
 -- user_0d_2 password should be set to user_0d_2_pwd
 -- database hbtn_0d_2 already exists, your script should not fail
 -- user user_0d_2 already exists, your script should not fail
-CREATE database IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
