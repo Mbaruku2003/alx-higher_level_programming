c37e1a835fafde38ac099c81302c314eac4cec81
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 08:47:57 -0700

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS states (
+CREATE table IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256));
