3315066d17086e2baf08856d7d2959b9bb75f724
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 12:56:39 -0700

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,5 @@
 SELECT name FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-WITH tv_shows.title = 'Dexter'
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name;
