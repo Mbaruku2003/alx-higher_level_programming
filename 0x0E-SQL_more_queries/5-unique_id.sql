a64b0d3ca91bffd815ec1629cb606cde1f5ad67c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 08:02:04 -0700

    5-unique_id.sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- creates the table unique_id
+-- unique_id descriptiond INTDEFAULT IS 1
+-- name VARCHAR(256)
+--table unique id
+CREATE table IF NOT EXISTS unique_id (id INT DEFAULT 1, name VARCHAR(256));
