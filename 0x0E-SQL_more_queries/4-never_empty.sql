76ab25ff362ca2d33daf2d707e561e07b54b365d
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 08:25:52 -0700

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -3,6 +3,6 @@
 -- and name VARCHAR(256)
 -- if the table id_not_null already exist its ok
 CREATE table IF NOT EXISTS id_not_null (
-	id INT DEFAULT 1, 
+	id INT DEFAULT 1,
 	name VARCHAR(256) NOT NULL
 );
