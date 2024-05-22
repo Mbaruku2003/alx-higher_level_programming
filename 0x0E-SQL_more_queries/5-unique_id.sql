e3cfb7a7e1d7305b7a55145e9ebd242cd1f11e3e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 08:16:36 -0700

    try

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,5 +2,4 @@
 -- unique_id descriptiond INTDEFAULT IS 1
 -- name VARCHAR(256)
 --table unique id
-CREATE table IF NOT EXISTS unique_id
-	(id INT DEFAULT 1, UNIQUE(ID), name VARCHAR(256));
+CREATE table IF NOT EXISTS unique_id (id INT DEFAULT 1, UNIQUE(ID), name VARCHAR(256));
