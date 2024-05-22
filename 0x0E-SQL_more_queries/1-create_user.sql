bdc9451671442d5dcd0b84e67587912d7e3d525c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 04:27:08 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- user_0d_1 should have all privileges
 -- user_0d_1 password should be set to user_0d_1_pwd
 -- user_0d_1 already exists, your script should not fail
-CREATE user IF NOT EXISTS user_0d_1
+CREATE IF NOT EXISTS 'user_0d_1';
 GRANT ALL
 ON *.*
 TO 'user_0d_1'@'localhost';
