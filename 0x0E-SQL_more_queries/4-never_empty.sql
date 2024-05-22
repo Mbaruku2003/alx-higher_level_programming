c48289e1b9b9077d8a3261735bf7fdbc0cf08f77
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-22 07:45:31 -0700

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates the table id_not_null
 --id_not_null description id INT with the default 1
 -- and name VARCHAR(256)
--- if the table id_not_null already exists, your script should not fail.
+-- if the table id_not_null already exist its ok
 CREATE table IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
