999da49a62771b7bcc5d15db39fd67a0b5a3f9d8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-14 01:27:20 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,15 +5,19 @@
 class Square:
     """defining an instant"""
     def __init__(self, size=0, position=(0, 0)):
-        self._size = size
-        self._position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
+        """Gets the valu of size."""
+
         return self._size
 
     @size.setter
     def size(self, value):
+        """Sets the value of size."""
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -23,10 +27,14 @@ class Square:
 
     @property
     def position(self):
+        """Gets the position."""
+
         return self._position
 
     @position.setter
     def position(self, value):
+        """Sets the position value."""
+
         if (not isinstance(value, tuple) or len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
@@ -34,9 +42,13 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Define the area."""
+
         return (self._size * self._size)
 
     def my_print(self):
+        """Prints in stdout the square with the character #."""
+
         if self._size == 0:
             print("")
             return
