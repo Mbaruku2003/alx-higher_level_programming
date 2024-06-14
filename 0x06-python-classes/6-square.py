8e905b646d47337f8bb83e877caf5e62d7fc6211
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-14 00:50:48 -0700

    all() is a function used to target all elements of a list or tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,12 @@
 class Square:
     """defining an instant"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self._size = size
+        self._position = position
 
     @property
     def size(self):
-        return self.__size
+        return self._size
 
     @size.setter
     def size(self, value):
@@ -19,11 +19,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            self._size = value
 
     @property
     def position(self):
-        return self.__position
+        return self._position
 
     @position.setter
     def position(self, value):
@@ -34,15 +34,13 @@ class Square:
         self.__position = value
 
     def area(self):
-        return (self.__size * self.__size)
+        return (self._size * self._size)
 
     def my_print(self):
-        if self.__size == 0:
+        if self._size == 0:
             print("")
             return
 
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print("", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        [print("") for i in range(0, self._position[1])]
+        for i in range(0, self._size):
+            print(" " * self._position[0] + "#" * self._size)
