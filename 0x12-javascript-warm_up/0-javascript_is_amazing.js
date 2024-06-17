242de7a9714e028f73fa69b3afb61963715ea48d
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-17 06:03:17 -0700

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-/* print “JavaScript is amazing”*/
-/* constant variable called myVar with the value “JavaScript is amazing” */
-/* use console.log(...) to print all output */
-/* You are not allowed to use var */
+/* print JavaScript is amazing */
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
