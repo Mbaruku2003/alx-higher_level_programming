838a05d31d00a25479933b17152e45a67597e66c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-17 05:54:12 -0700

    console.log is used to print in javascript and const means the value wont change

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* print “JavaScript is amazing”*/
+/* constant variable called myVar with the value “JavaScript is amazing” */
+/* use console.log(...) to print all output */
+/* You are not allowed to use var */
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
