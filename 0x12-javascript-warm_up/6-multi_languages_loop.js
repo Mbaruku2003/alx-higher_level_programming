1a58a1e7cde8faed6c53d6157c23ee386a85c280
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-17 09:54:12 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 /*  prints 3 lines but by using an array of string and a loop */
 const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const items in list) {
-  console.log(items);
+  console.log(list[items]);
 }
