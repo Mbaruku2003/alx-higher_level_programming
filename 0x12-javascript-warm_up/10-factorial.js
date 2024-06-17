ded85df8bda9fc18c46c139ddc63621058b3a3f5
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-17 13:18:56 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* computes and prints a factorial */
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+console.log(Number(factorial(process.argv[2])));
