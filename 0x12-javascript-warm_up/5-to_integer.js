98194f51f03a6f9451e015a88a1c237d54cade8e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-17 09:15:52 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@ if (process.argv[2] === undefined || isNaN(process.argv[2]))
 }
 else
 {
-	console.log('My number: ', parseInt(process.argv[2]));
+	console.log('My number:', parseInt(process.argv[2]));
 }
