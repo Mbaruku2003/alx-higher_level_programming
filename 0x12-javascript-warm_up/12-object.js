94ba286299949c85b99274e6925be95e33d76172
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-17 13:54:58 -0700

    13- we were exporting files to another file through request

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* replace the value 12 with 89 */
 const isobject = {
-	type: 'object',
-	value: 12
+  type: 'object',
+  value: 12
 };
 console.log(isobject);
 isobject.value = 89;
