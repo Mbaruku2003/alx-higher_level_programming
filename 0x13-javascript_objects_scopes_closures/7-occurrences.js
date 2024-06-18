55d1f60dae3eecf2b120f816f0ca8225701130f2
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-18 12:27:45 -0700

    .reducer() is used to execute a reducer function on each element of an array

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 /* returns the number of occurrences in a list */
 exports.nbOccurences = function (list, searchElement) {
-	return list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0);
-}
+  return list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0);
+};
