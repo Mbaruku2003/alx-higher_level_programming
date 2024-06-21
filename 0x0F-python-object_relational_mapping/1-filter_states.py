f2446a7b1d03b3b057476f3aef94bf75b1d4a39c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-21 03:30:49 -0700

    From states we imported everythingwhere name like binary starts with "N%" that is where binary starts with n and then orderd by states.id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N (upper N) from hbtn_0e_0_usa. """
+""" lists all states with a name starting with N (upper N). """
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user_name=sys.argv[1],
-                        passwd=sys.argv[2], dbname=sys.argv[3], posrt=3306)
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
     c.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
