56439c7bcc22cc206920de1b9df13aed5a44f600
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-21 06:12:53 -0700

    we used equals to show that a variable has a certain value

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" lists all cities from the database. """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    serverconn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    C = serverconn.cursor()
+    C.execute("SELECT cities.id, cities.name states.name FROM
+              cities INNER JOIN states ON states.id=cities.state_id")
+    rows = C.fetchall()
+    for row in rows:
+        print(row)
+    C.close()
+    serverconn.close()
