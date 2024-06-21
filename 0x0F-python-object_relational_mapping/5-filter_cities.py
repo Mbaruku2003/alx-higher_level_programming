f6a2a17b36e26e1f6a37595226dd8c13feff0f82
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-21 08:11:03 -0700

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  takes in the name of a state as an argument and lists. """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    C = db.cursor()
+    C.execute("""SELECT cities.name FROM
+              cities INNER JOIN states ON states.id=cities.states_id
+              WHERE states.name=%s""", (sys.argv[4], ))
+    rows = C.fetchall()
+    temporary = list(row[0] for row in rows)
+    print(*temporary, sep=", ")
+    C.close()
+    db.close()
