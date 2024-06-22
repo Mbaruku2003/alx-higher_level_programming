189cfcbd4d2fbe5fdbec59da79a241c9cf5e4caf
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-22 09:49:13 -0700

    create_engine is used to create a connection, sessionmaker is used to consigure the code, base.metacata.create_all() is used to create tables from base and session is used to create a session, query is used to add a query

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """lists all State objects from the database hbtn_0e_6_usa."""
 import sys
-from model_base import Base, State
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
