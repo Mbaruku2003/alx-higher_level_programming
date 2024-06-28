bbbec4af0caaba08ae1a54f845015bc0f69f108c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-28 09:39:12 -0700

    trial

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument"""
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    import sys
+    from model_state import State, Base
+
+    inputs = sys.argv
+    if len(inputs) < 5 or ";" in inputs[4]:
+        exit(1)
+
+    connection_string = 'mysql+mysqldb://():()@localhost:3306/{}'
+    engine = create_engine(connection_string.format
+                           (inputs[4], inputs[2], inputs[3]))
+    Session = sessionmaker(engine)
+
+    Base.metadata.create_all(engine)
+    session = Session()
+    my_query = session.query(State).filter(State.name.like(inputs[4])).all()
+    if len(my_query) == 0:
+        print('Not found')
+    else:
+        print(my_query[0].id)
+session.close()
