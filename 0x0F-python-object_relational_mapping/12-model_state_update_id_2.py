b2044f0910b5f4c01d6f9e61e71da68cd1669eb5
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-28 13:37:48 -0700

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instances = session.query(State).filter_by(id=2), first()
+    new_instances = session.query(State).filter_by(id=2).first()
     new_instances.name = 'New Mexico'
     session.commit()
