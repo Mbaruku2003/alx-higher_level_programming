dbba7129630df7dd95eae24198cb34cc90d74aba
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-06 14:31:42 -0700

    created_python file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,4 +8,4 @@ url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
     headers = response.headers
     x_requested_id = headers.get('X-Request-Id')
-print(x_requested_id)
+    print(x_requested_id)
