3096916a7eb1fbe81f8f8f658c747c93f936c796
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-06 14:41:27 -0700

    created_python file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """takes in a URL, sends a request to the URL and displays the value."""
-import urllib
+import urllib.request
 import sys
 
 
@@ -8,4 +8,4 @@ url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
     headers = response.headers
     x_requested_id = headers.get('X-Request-Id')
-    print(x_requested_id)
+print(x_requested_id)
