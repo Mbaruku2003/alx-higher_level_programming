cac3d4769603f3bc7a3c12c0c7f2a85e82b1ab9f
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-06 13:57:43 -0700

    created_python file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html_body = response.read()
-print("Body respone:")
+print("Body response:")
 print("\t- type:", type(html_body))
 print("\t- content:", html_body)
 print("\t- utf8 content:", html_body.decode('utf-8'))
