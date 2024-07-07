85ea61aed768ac6781311dfd8674d093d6ae2493
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-07 07:40:34 -0700

    created_python file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request to a url. """
+import requests
+import sys
+
+
+def search_for_user(letter):
+    payload = {'q': letter}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    try:
+        result = response.json()
+        if result:
+            print(f"[{result.get('id')}] {result.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__manin__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    search_user(letter)
