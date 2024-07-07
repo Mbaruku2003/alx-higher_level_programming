8202fd7700f7bc3aab9fd894432a116c3a84ba68
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-07 03:41:17 -0700

    created_python file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the value."""
+""" Takes in a URL, sends a request to the URL and displays the value. """
 import urllib.request
 import sys
 
