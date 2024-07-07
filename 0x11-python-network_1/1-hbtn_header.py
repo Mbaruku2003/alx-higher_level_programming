a6e2dce4d849e9676e8b42f5f5fe98b99b1755ca
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-07 03:53:06 -0700

    created_python file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Takes in a URL, sends a request to the URL and displays the value. """
+
+
 import urllib.request
 import sys
-
-
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
     headers = response.headers
