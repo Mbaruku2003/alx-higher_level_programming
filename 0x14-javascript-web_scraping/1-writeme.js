a2633d5711c0b3b9cc95933824bd5dc67276c3f4
Mbauku2003 <leonardjembe21@gmail.com>
2024-07-30 16:33:54 +0300

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+const thefile = process.argv(2);
+const content = process.argv(3);
+if (!content || !thefile) {
+  console.error();
+  process.exit(1);
+}
+fs.readFile(thefile, utf - 8, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(content);
+  }
+});
