42efecb3b602b823c1f9ae83664a9c818f0de806
Mbauku2003 <leonardjembe21@gmail.com>
2024-07-30 19:27:18 +0300

    updated the file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 if (process.argv.length !== 4) {
-	process.exit(1);
+  process.exit(1);
 }
 const filepath = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(filepath, content, 'utf8', (err) => {
-	if (err) {
-		console.error(err);
-	}
+  if (err) {
+    console.error(err);
+  }
 });
